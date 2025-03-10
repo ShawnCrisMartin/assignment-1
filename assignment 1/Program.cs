45495834f048d28774b8cea98816253da26111d6
Dell <Dell@DESKTOP-V36L15C>
2025-03-10 20:20:44 +0000

    Add project files.

---
+++ assignment 1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Media;
+using System.Numerics;
+
+namespace DungeonExplorer
+{
+    internal class Game
+    {
+        private Player player;
+        private Room currentRoom;
+
+        public Game()
+        {
+            // Initialize the game with one room and one player
+
+        }
+        public void Start()
+        {
+            // Change the playing logic into true and populate the while loop
+            bool playing = false;
+            while (playing)
+            {
+                // Code your playing logic here
+            }
+        }
+    }
+}
