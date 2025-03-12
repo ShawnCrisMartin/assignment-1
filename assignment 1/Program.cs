a04ce0c485606595a6b445b32527f8e2291d3eac
ShawnCrisMartin <shawncris@gmail.com>
2025-03-12 17:10:26 +0000

    error handling attempt 2

---
+++ assignment 1/Program.cs
@@ -0,0 +1,17 @@
+using DungeonExplorer;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Game game = new Game();
+            game.Start();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
