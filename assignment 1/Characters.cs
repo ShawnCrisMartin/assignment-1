8db3048edd10d677a52a543a1d6a8dfa017e696f
ShawnCrisMartin <shawncris@gmail.com>
2025-04-25 17:57:42 +0100

    fixing character protection issue

--- assignment 1/Characters.cs
+++ assignment 1/Characters.cs
@@ -4,7 +4,7 @@
     {
         // Made protected so that derived classes can access it
         public string Name { get; set; }
-        protected int Health { get; set; }
+        public int Health { get; set; }
 
         // Constructor to initialize Name and Health
         public Character(string name, int health)
